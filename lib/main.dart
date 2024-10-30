19f1f84ff89fc57098e69a8fffe3dc3a2ae688d8
DASHU <385321165@qq.com>
2024-10-30 10:23:04 +0800

    git init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:nostr_sdk/relay_local/relay_local_db.dart';
+import 'package:relay_sdk/relay_manager.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  RelayLocalDB.configWindowsSqlite();
+
   runApp(const MyApp());
 }
 
@@ -57,15 +62,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  RelayManager relayManager = RelayManager();
+
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    relayManager.start();
   }
 
   @override
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import cryptography_flutter
+import device_info_plus
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }

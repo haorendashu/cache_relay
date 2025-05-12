024c0085bb3ef3d63839c6d8dc44b948f5eabdd4
DASHU <385321165@qq.com>
2025-05-13 01:45:22 +0800

    chagne get ip method

--- lib/util/ip_util.dart
+++ lib/util/ip_util.dart
@@ -1,18 +1,19 @@
-
 import 'dart:io';
 
 class IpUtil {
-
-  static Future<String?>  getIp() async {
+  static Future<String?> getIp() async {
     var ips = await NetworkInterface.list();
     for (var interface in ips) {
       print('== Interface: ${interface.name} ==');
-      for (var addr in interface.addresses) {
-        print(
-            '${addr.address} ${addr.host} ${addr.isLoopback} ${addr.rawAddress} ${addr.type.name}');
-        return addr.address;
+      if ((interface.name.toLowerCase()).contains("wlan")) {
+        for (var addr in interface.addresses) {
+          print(
+              '${addr.address} ${addr.host} ${addr.isLoopback} ${addr.rawAddress} ${addr.type.name}');
+          return addr.address;
+        }
       }
     }
+
+    return ips.first.addresses.first.address;
   }
-  
 }
